24679e865b183231d0d18be371636b6f34263a03
Phil Nazarov <artem.nazarov@su.suffolk.edu>
2026-03-05 13:33:07 -0500

    Enhance backend_functions with spacy lookups and delay
    Added spacy_lookups_data and time imports, and implemented a directory search for lemma files.

--- backend_functions.py
+++ backend_functions.py
@@ -28,7 +28,16 @@ def extract_words(input_file: str, output_folder: str, progress = None) -> str:
 
     report("\n🔄 Loading the neural network.")
     try:
-        import os, sys, spacy
+        import os, sys, spacy, spacy_lookups_data, time
+
+        report(spacy_lookups_data.__file__)
+        time.sleep(30)
+        root = os.path.dirname(spacy_lookups_data.__file__)
+        for directory_path, directories_names, file_names in os.walk(root):
+            for file in file_names:
+                if 'lemma' in file.lower():
+                    report(os.path.join(directory_path, file))
+        
         from pathlib import Path
         def _model_path() -> Path:
             if getattr(sys, 'frozen', False):
